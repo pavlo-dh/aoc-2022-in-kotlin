5e122b161402bd4f563cd856ea6601968a8867b1
Pavlo Gotsonoga <pavlo.gotsonoga@deliveryhero.com>
2022-12-01 12:59:49 +0100

    Day 1.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,32 @@
 fun main() {
+    fun calculateElvesCalories(input: List<String>): List<Int> {
+        val elvesCalories = arrayListOf<Int>()
+        var currentElfCalories = 0
+        input.forEach {
+            if (it.isEmpty()) {
+                elvesCalories.add(currentElfCalories)
+                currentElfCalories = 0
+            } else {
+                currentElfCalories += it.toInt()
+            }
+        }
+        elvesCalories.add(currentElfCalories)
+        return elvesCalories
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val elvesCalories = calculateElvesCalories(input)
+        return elvesCalories.max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val elvesCalories = calculateElvesCalories(input)
+        return elvesCalories.sortedDescending().take(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
